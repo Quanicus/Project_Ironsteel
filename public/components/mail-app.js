247128e73476660737a77a5088a3d4fef43eb535
Quan Pham <quanicus@Quans-MacBook-Pro-2.local>
2025-02-22 16:11:24 -0600

    removed initial check for nav entry

--- public/components/mail-app.js
+++ public/components/mail-app.js
@@ -421,13 +421,11 @@ class MailApp extends HTMLElement {
         this.getSentMessages();
         this.activateMessagePreviews();
         
-        if (this.nav.entries[0]) {
-            this.nav.entries[0].dispatchEvent.click();
-            console.log("clicked");
-        } else {
-            console.log("displaying recieved messages");
+
+            this.nav.entries[0].dispatchEvent(new Event("click"));
+
             this.displayMessagePreviews(this.recievedMessagePreviews);
-        }  
+        
     }
 
     updateMessageDisplay(message = this.selectedMessage) {
